f9fa8c9d44ae8f9818c3bb85e8bfb8545e226cac
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-05-13 16:05:57 +0300

    Scaffolded Identity through the soution menu so as to have the option to apply custom configurations

---
+++ RestApi.NetCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestApi.NetCore.Data;
+
+[assembly: HostingStartup(typeof(RestApi.NetCore.Areas.Identity.IdentityHostingStartup))]
+namespace RestApi.NetCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
