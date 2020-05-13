67253114cdac85f1a77d18025bbb90b83c78cb1b
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-05-13 15:42:45 +0300

    Add project files.

---
+++ RestApi.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestApi.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
