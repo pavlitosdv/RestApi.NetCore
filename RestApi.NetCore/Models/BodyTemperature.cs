cb3f848771bbf5056a0d7c68876af53707cfef22
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-05-13 23:35:24 +0300

    Added Model class -> BodyTemperature with some properties

---
+++ RestApi.NetCore/Models/BodyTemperature.cs
@@ -0,0 +1,22 @@
+﻿using RestApi.NetCore.Areas.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.NetCore.Models
+{
+    public class BodyTemperature
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Temperature { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
