e6132429f9d889e6d00e46ff6934bf628b286f61
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-05-15 02:53:31 +0300

    Added Interfaces Folder with two interfaces 1) IBodyTemperaturesIntreface 2) IFeverIntervalInterface with their methods

---
+++ RestApi.NetCore/Interfaces/IBodyTemperaturesIntreface.cs
@@ -0,0 +1,23 @@
+﻿using RestApi.NetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.NetCore.Interfaces
+{
+    interface IBodyTemperaturesIntreface
+    {
+
+        Task<IEnumerable<BodyTemperature>> GetAllBodyTemperatures();
+        Task<BodyTemperature> GetBodyTemperaturesById(int id);
+
+        //Task<IActionResult> PutBodyTemperature(int id, BodyTemperature bodyTemperature);
+
+        Task<BodyTemperature> AddBodyTemperature(BodyTemperature bodyTemperature);
+
+        //bool SaveAll();
+
+        Task<BodyTemperature> DeleteBodyTemperature(int id);
+    }
+}
---
+++ RestApi.NetCore/Interfaces/IFeverIntervalInterface.cs
@@ -0,0 +1,13 @@
+﻿using RestApi.NetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.NetCore.Interfaces
+{
+    interface IFeverIntervalInterface
+    {
+        void FeverIntervalMethod(BodyTemperature bodyTemperature);
+    }
+}
