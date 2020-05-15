07097b53c68d4205dd37ff89b18c72edcf752a51
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-05-15 12:52:33 +0300

    Implementations of Interfaces were made into the 1) BodyTemperatureRepository 2) FeversIntervalRepository

--- RestApi.NetCore/Repositories/BodyTemperatureRepository.cs
+++ RestApi.NetCore/Repositories/BodyTemperatureRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RestApi.NetCore.Data;
+using RestApi.NetCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,59 @@ namespace RestApi.NetCore.Repositories
 {
     public class BodyTemperatureRepository
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public BodyTemperatureRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BodyTemperature>> GetAllBodyTemperatures()
+        {
+            IEnumerable<BodyTemperature> bodyTemperatures;
+
+            bodyTemperatures = await _context.BodyTemperatures.ToListAsync();
+
+            //.Cast<BodyTemperatureRepository>()
+
+            return bodyTemperatures;
+        }
+
+        public async Task<BodyTemperature> GetBodyTemperaturesById(int id)
+        {
+            var bodyTemperature = await _context.BodyTemperatures.FindAsync(id);
+
+            if (bodyTemperature == null)
+            {
+                return bodyTemperature;
+            }
+
+            return bodyTemperature;
+        }
+
+        public async Task<BodyTemperature> AddBodyTemperature(BodyTemperature bodyTemperature)
+        {
+
+            _context.BodyTemperatures.Add(bodyTemperature);
+            await _context.SaveChangesAsync();
+            return bodyTemperature;
+        }
+
+        public async Task<BodyTemperature> DeleteBodyTemperature(int id)
+        {
+            var bodyTemperature = await _context.BodyTemperatures.FindAsync(id);
+            if (bodyTemperature == null)
+            {
+                return bodyTemperature;
+            }
+
+            _context.BodyTemperatures.Remove(bodyTemperature);
+            await _context.SaveChangesAsync();
+
+            return bodyTemperature;
+        }
+
+
     }
 }
--- RestApi.NetCore/Repositories/FeversIntervalRepository.cs
+++ RestApi.NetCore/Repositories/FeversIntervalRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestApi.NetCore.Data;
+using RestApi.NetCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,80 @@ namespace RestApi.NetCore.Repositories
 {
     public class FeversIntervalRepository
     {
+        private readonly ApplicationDbContext _context;
+        public FeversIntervalRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void FeverIntervalMethod(BodyTemperature bodyTemperature)
+        {
+            FeverInterval fever = GetUserLastFeverInterval(bodyTemperature.UserId);
+
+
+            if (bodyTemperature.Temperature > 37.5)
+            {
+                if (fever == null)
+                {
+                    FeverInterval feverInterval = new FeverInterval();
+                    feverInterval.UserId = bodyTemperature.UserId;
+                    feverInterval.StartedTemperature = bodyTemperature.Temperature;
+                    feverInterval.StartDate = DateTime.Now.Date;
+                    //feverInterval.EndDate = null;
+
+                    PostFeverInterval1(feverInterval);
+                }
+                else if (fever.EndDate != null)
+                {
+                    FeverInterval feverInterval = new FeverInterval();
+                    feverInterval.UserId = bodyTemperature.UserId;
+                    feverInterval.StartedTemperature = bodyTemperature.Temperature;
+                    feverInterval.StartDate = DateTime.Now.Date;
+                    //feverInterval.EndDate = null;
+
+                    PostFeverInterval1(feverInterval);
+                }
+            }
+            else
+            {
+                if (fever != null && fever.EndDate == null)
+                {
+                    FeverInterval feverInterval = new FeverInterval();
+                    feverInterval.Id = fever.Id;
+                    feverInterval.UserId = fever.UserId;
+                    feverInterval.StartedTemperature = fever.StartedTemperature;
+                    feverInterval.StartDate = fever.StartDate;
+                    feverInterval.EndDate = DateTime.Now.Date;
+                    PutFeverInterval(fever.Id, feverInterval);
+                }
+
+            }
+
+        }
+
+        public FeverInterval GetUserLastFeverInterval(string userId)
+        {
+            var feverInterval = _context.FeverIntervals.AsNoTracking().AsEnumerable().LastOrDefault(i => i.UserId == userId);
+
+            return feverInterval;
+        }
+
+
+        public void PostFeverInterval1(FeverInterval feverInterval)
+        {
+            _context.FeverIntervals.Add(feverInterval);
+            //await _context.SaveChangesAsync();
+        }
+
+
+        public void PutFeverInterval(int id, FeverInterval feverInterval)
+        {
+            _context.FeverIntervals.Attach(feverInterval);
+            _context.Entry(feverInterval).State = EntityState.Modified;
+            // _context.SaveChangesAsync();            
+
+        }
+
     }
 }
